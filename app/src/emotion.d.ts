05d4b1cc5734965afbc690e362434d63e2d80619
dinhwe2612 <manh5dinh29@gmail.com>
2025-03-29 19:57:23 +0700

    minor fix bug

--- app/src/emotion.d.ts
+++ app/src/emotion.d.ts
@@ -1,5 +1,5 @@
 import '@emotion/react';
-import { Theme as MuiTheme } from '@material-ui/core/styles/createMuiTheme';
+import { Theme as MuiTheme } from '@material-ui/core/styles';
 
 declare module '@emotion/react' {
   export interface Theme extends MuiTheme {}
